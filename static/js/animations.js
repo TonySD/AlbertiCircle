b88e7c6de057b0d82ebc8edbaec790a6237a68ea
Anton Vaghin <asvagin_2@edu.hse.ru>
2024-06-01 02:26:51 +0300

    Impl: some code decorations

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,73 +1,72 @@
 const textinput = document.getElementById("textinput");
-      const keyinput = document.getElementById("keyinput");
-      const passinput = document.getElementById("passinput");
-      const indicatorinput = document.getElementById("indicatorinput");
+const keyinput = document.getElementById("keyinput");
+const passinput = document.getElementById("passinput");
+const indicatorinput = document.getElementById("indicatorinput");
 
-      const mode1Radio = document.getElementById("mode1Radio");
-      const mode2Radio = document.getElementById("mode2Radio");
-      const mode3Radio = document.getElementById("mode3Radio");
-      const mode4Radio = document.getElementById("mode4Radio");
-      const mode5Radio = document.getElementById("mode5Radio");
+function resetAllFields() {
+    textinput.style.display = "none";
+    keyinput.style.display = "none";
+    passinput.style.display = "none";
+    indicatorinput.style.display = "none";
+}
 
-      mode1Radio.addEventListener("change", () => {
-        if (mode1Radio.checked) {
-          textinput.style.display = "block";
-          keyinput.style.display = "block";
-          passinput.style.display = "none";
-          indicatorinput.style.display = "none";
-        }
-      });
+function enableFields(elements) {
+    resetAllFields();
+    elements.forEach(function(element) {
+        element.style.display = "block";
+    });
+}
 
-      mode2Radio.addEventListener("change", () => {
-        if (mode2Radio.checked) {
-          textinput.style.display = "block";
-          keyinput.style.display = "block";
-          passinput.style.display = "none";
-          indicatorinput.style.display = "none";
-        }
-      });
+const mode1Radio = document.getElementById("mode1Radio");
+const mode2Radio = document.getElementById("mode2Radio");
+const mode3Radio = document.getElementById("mode3Radio");
+const mode4Radio = document.getElementById("mode4Radio");
+const mode5Radio = document.getElementById("mode5Radio");
 
-      mode3Radio.addEventListener("change", () => {
-        if (mode3Radio.checked) {
-          textinput.style.display = "block";
-          keyinput.style.display = "block";
-          passinput.style.display = "none";
-          indicatorinput.style.display = "block";
-        }
-      });
+mode1Radio.addEventListener("change", () => {
+    if (mode1Radio.checked) {
+        enableFields([textinput, keyinput]);
+    }
+});
 
-      mode4Radio.addEventListener("change", () => {
-        if (mode4Radio.checked) {
-          textinput.style.display = "block";
-          keyinput.style.display = "block";
-          passinput.style.display = "none";
-          indicatorinput.style.display = "none";
-        }
-      });
+mode2Radio.addEventListener("change", () => {
+    if (mode2Radio.checked) {
+        enableFields([textinput, keyinput]);
+    }
+});
 
-      mode5Radio.addEventListener("change", () => {
-        if (mode5Radio.checked) {
-          textinput.style.display = "block";
-          keyinput.style.display = "block";
-          passinput.style.display = "block";
-          indicatorinput.style.display = "none";
-        }
-      });
+mode3Radio.addEventListener("change", () => {
+    if (mode3Radio.checked) {
+        enableFields([textinput, keyinput, indicatorinput]);
+    }
+});
 
-      const keychoose1 = document.getElementById("keychoose1");
-      const keychoose2 = document.getElementById("keychoose2");
-      const keyinput1 = document.getElementById("keyinput1");
-      const keyinput2 = document.getElementById("keyinput2");
+mode4Radio.addEventListener("change", () => {
+    if (mode4Radio.checked) {
+        enableFields([textinput, keyinput]);
+    }
+});
 
-      keychoose1.addEventListener("change", () => {
-        if (keychoose1.checked) {
-          keyinput2.disabled = true;
-        }
-      });
+mode5Radio.addEventListener("change", () => {
+    if (mode5Radio.checked) {
+        enableFields([textinput, keyinput, passinput]);
+    }
+});
 
-      keychoose2.addEventListener("change", () => {
-        if (keychoose2.checked) {
-          keyinput2.disabled = false;
-          keyinput2.focus();
-        }
-      });
+// const keychoose1 = document.getElementById("keychoose1");
+// const keychoose2 = document.getElementById("keychoose2");
+// const keyinput1 = document.getElementById("keyinput1");
+// const keyinput2 = document.getElementById("keyinput2");
+
+// keychoose1.addEventListener("change", () => {
+//     if (keychoose1.checked) {
+//         keyinput2.disabled = true;
+//     }
+// });
+
+// keychoose2.addEventListener("change", () => {
+//     if (keychoose2.checked) {
+//         keyinput2.disabled = false;
+//         keyinput2.focus();
+//     }
+// });
--- static/js/formHandler.js
+++ static/js/formHandler.js
@@ -1,33 +1,44 @@
-const myForm = document.getElementById("myForm");
+const myForm = document.getElementById("cipher");
 
 myForm.addEventListener("submit", async (event) => {
-event.preventDefault(); // Предотвращаем стандартное действие отправки формы
+  event.preventDefault(); // Предотвращаем стандартное действие отправки формы
 
-const formData = new FormData(myForm);
-const formObj = Object.fromEntries(formData.entries());
-const keychoose2 = document.getElementById("keychoose2");
+  const formData = new FormData(myForm);
+  const formObj = Object.fromEntries(formData.entries());
+  if (formObj.keychoose == "0") {
+	formObj.key = document.getElementById("keyinput1").value;
+  }
+  if (formObj.keychoose == "1") {
+	formObj.key = document.getElementById("keyinput2").value;
+  }
 
-delete formObj.keychoose;
+	delete formObj.keychoose;
+//   const keychoose2 = document.getElementById("keychoose2_btn");
 
-if (!keychoose2.checked) {
-    delete formObj.keyinput2;
-}
+//   delete formObj.keychoose;
 
-const requestOptions = {
+//   if (!keychoose2.checked) {
+//     delete formObj.keyinput2;
+//   }
+
+  const requestOptions = {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(formObj),
-};
-response = NaN;
-try {
-    response = await fetch("/mode/", requestOptions);
+  };
+  response = NaN;
+  try {
+    response = await fetch(
+      "/mode/",
+      requestOptions
+    );
     response = await response.json();
-} catch (error) {
+  } catch (error) {
     console.error("Ошибка:", error);
-}
+  }
 
-const resultDiv = document.createElement("div");
-resultDiv.innerHTML = `
+  const resultDiv = document.createElement("div");
+  resultDiv.innerHTML = `
     <h2>Результат запроса:</h2>
     <p><strong>Шифр-текст:</strong> ${response.ciphertext}</p>
     <p><strong>Ключ:</strong> ${response.key}</p>
@@ -35,5 +46,5 @@ resultDiv.innerHTML = `
     <p><strong>Индикаторная буква:</strong> ${response.indicator}</p>
     <p><strong>Результат:</strong> ${response.plaintext} </p>
 `;
-document.body.appendChild(resultDiv);
+  document.body.appendChild(resultDiv);
 });
